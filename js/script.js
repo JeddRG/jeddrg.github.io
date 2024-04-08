2c46ae3767408ab69f7efc47b99b470e5b2a6c31
JeddRG <113360236+JeddRG@users.noreply.github.com>
2024-04-08 14:47:54 +0100

    javascript being implemented
    implemented the randomiser.
    need to change the words/ have the scramble background be bigger as bigger words are changing the size too much

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-const initGame = () => {
-    let randomObj = words[Math.floor(Math.random()*words.length)]; //getting random object from words
-    console.log(randomObj);
+const wordText = document.querySelector(".word");
+
+const wordSelector = () => {
+    let rand = words[Math.floor(Math.random()*words.length)]; //this will select a random word from our words.js file
+    let word = rand.word.split(""); //splits the chosen string into characters
+
+    for (let i = word.length - 1; i> 0; i--){
+        let j = Math.floor(Math.random()* (i+1));
+        [word[i], word[j]] = [word[j], word[i]];
+    }
+    // this is a loop that will swap the positions of the characters with eachother
+    // i is the position of a character and it will go from the last letter to the first letter for every lenght of word
+    // j is going to be a random letter than isnt i (i + 1 means that j != i)
+    // we then switch the positions of i and j which scrambles our word
+    wordText.innerText = word;
 }
-initGame();
+wordSelector();
