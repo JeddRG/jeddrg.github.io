476b7e4230087050c5ad8932f511a121b3ff7efa
JeddRG <113360236+JeddRG@users.noreply.github.com>
2024-04-08 15:08:19 +0100

    implemented the refresh button
    after too many tries

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 const wordText = document.querySelector(".word");
 
+//HTML buttons elements
+const refreshButton = document.getElementById("refresh");
+const submitButton = document.getElementById("submit");
+
 const wordSelector = () => {
     let rand = words[Math.floor(Math.random()*words.length)]; //this will select a random word from our words.js file
     let word = rand.word.split(""); //splits the chosen string into characters
+
 
     for (let i = word.length - 1; i> 0; i--){
         let j = Math.floor(Math.random()* (i+1));
@@ -13,5 +18,13 @@ const wordSelector = () => {
     // j is going to be a random letter than isnt i (i + 1 means that j != i)
     // we then switch the positions of i and j which scrambles our word
     wordText.innerText = word;
+
+
+    //button functionality
+    //refresh
+    function handleClick() {
+        window.location.reload();
+    }
+    refreshButton.addEventListener("click", handleClick);
 }
 wordSelector();
