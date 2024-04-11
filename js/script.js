a936469179b5dcc037d560e85a70f217f86920a9
JeddRG <113360236+JeddRG@users.noreply.github.com>
2024-04-11 13:16:57 +0100

    functionalising the randomising

--- js/script.js
+++ js/script.js
@@ -9,17 +9,34 @@ const wordSelector = () => {
     let rand = words[Math.floor(Math.random()*words.length)]; //this will select a random word from our words.js file
     let word = rand.word.split(""); //splits the chosen string into characters
 
+    function randomise(word){
 
-    for (let i = word.length - 1; i> 0; i--){
-        let j = Math.floor(Math.random()* (i+1));
-        [word[i], word[j]] = [word[j], word[i]];
+        for (let i = word.length - 1; i> 0; i--){
+            let j = Math.floor(Math.random()* (i+1));
+            [word[i], word[j]] = [word[j], word[i]];
+            wordText.innerText = word;
+            correctWord = rand;
+        }
+        return correctWord;
     }
+
+    // for (let i = word.length - 1; i> 0; i--){
+    //     let j = Math.floor(Math.random()* (i+1));
+    //     [word[i], word[j]] = [word[j], word[i]];
+    // }
+
+
+
+
     // this is a loop that will swap the positions of the characters with eachother
     // i is the position of a character and it will go from the last letter to the first letter for every lenght of word
     // j is going to be a random letter than isnt i (i + 1 means that j != i)
     // we then switch the positions of i and j which scrambles our word
-    wordText.innerText = word;
-    correctWord = rand;
+
+
+    
+    // wordText.innerText = word;
+    // correctWord = rand;
 }
 wordSelector();
 
