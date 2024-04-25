747fbd6030118e2b62105ca04335a4728a9e6b3e
JeddRG <113360236+JeddRG@users.noreply.github.com>
2024-04-25 15:19:39 +0100

    put in more words
    15 of each, 45 total words to guess

--- js/words.js
+++ js/words.js
@@ -2,12 +2,17 @@
 // This is a website which will host an anagram game called "Scramble"
 // These are the arrays holding the words for the scramble
 const words = [
-    {word: "piano"}, {word: "socks"}, {word: "pizza"}, {word: "jerky"}, {word: "quiff"}, {word: "crazy"},
-    {word: "field"}, {word: "zebra"}, {word: "unzip"}, {word: "chuck"}, {word: "banjo"}, {word: "japan"}
+    {word: "piano"}, {word: "socks"}, {word: "pizza"}, {word: "jerky"}, {word: "quiff"}, 
+    {word: "crazy"}, {word: "field"}, {word: "zebra"}, {word: "unzip"}, {word: "chuck"}, 
+    {word: "banjo"}, {word: "japan"}, {word: "cabin"}, {word: "early"}, {word: "caged"}
 ]
 const sixwords = [
-    {sixword: "kebabs"}, {sixword: "eagles"}, {sixword: "fabric"}, {sixword: "habits"}, {sixword: "jabbed"}, {sixword: "nachos"}
+    {sixword: "kebabs"}, {sixword: "eagles"}, {sixword: "fabric"}, {sixword: "habits"}, {sixword: "jabbed"}, 
+    {sixword: "nachos"}, {sixword: "abroad"}, {sixword: "accuse"}, {sixword: "babies"}, {sixword: "badger"},
+    {sixword: "buyers"}, {sixword: "falcon"}, {sixword: "hacked"}, {sixword: "macros"}, {sixword: "maggot"}
 ]
 const sevwords = [
-    {sevword: "jacuzzi"}, {sevword: "quizzes"}, {sevword: "buzzsaw"}, {sevword: "jukebox"}, {sevword: "cabbage"}, {sevword: "earache"}
+    {sevword: "jacuzzi"}, {sevword: "quizzes"}, {sevword: "buzzsaw"}, {sevword: "jukebox"}, {sevword: "cabbage"}, 
+    {sevword: "earache"}, {sevword: "bagpipe"}, {sevword: "baklava"}, {sevword: "jogging"}, {sevword: "jackpot"}, 
+    {sevword: "objects"}, {sevword: "observe"}, {sevword: "rabbits"}, {sevword: "sabbath"}, {sevword: "ukelele"}
 ]
